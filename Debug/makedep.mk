e7a03d961e32add7ac67f47b2629cd4c4f81f031
Saroj Shakya <sshakya@kathmandu>
2019-11-14 14:39:22 +0545

    a

---
+++ Debug/makedep.mk
@@ -0,0 +1,6 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+Home Security Final Code with Message Read.c
+
--- Home Security Final Code with Message Read/Home Security Final Code with Message Read/Debug/makedep.mk
+++
@@ -1,6 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit or delete the file
-################################################################################
-
-Home Security Final Code with Message Read.c
-
